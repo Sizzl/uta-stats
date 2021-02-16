d5e6547886606847b57a4d2c433e08e0100f2b7a
Sizzl <sizzl@utassault.net>
2021-02-16 20:41:49 +0000

    Updating ranking recalc handler

--- htdocs/import/import_ranking.php
+++ htdocs/import/import_ranking.php
@@ -7,7 +7,7 @@ if (!isset($gamename))
 if ($playerbanned)
 	return;
 
-if (!isset($rank_year))
+if (!isset($rank_year) || $rank_year == 0)
 {
 	$rank_year = 0;
 	$rank_time_start = 0;
@@ -42,9 +42,17 @@ if (strpos($gamename, 'Assault') !== false)
 
 	$ass_att = $r_cnt['ass_att']; 
 	$ass_def = $r_cnt['ass_def'];
-	$ratio_att = intval($ass_att * 100 / ($ass_att + $ass_def)); // add checks
-	$ratio_def = 100 - $ratio_att;
-										
+
+	if ($ass_att+$ass_def == 0)
+	{
+		echo "<pre>".$sql."</pre>";
+		break;
+	}
+	else
+	{
+		$ratio_att = intval($ass_att * 100 / ($ass_att + $ass_def)); // add checks
+		$ratio_def = 100 - $ratio_att;
+	}									
 	// Fragging Events
 	$rank_fpos = $r_cnt[frags]+$r_cnt[spree_double]+$r_cnt[spree_multi]+$r_cnt[spree_ultra]+$r_cnt[spree_monster]+$r_cnt[spree_kill]+$r_cnt[spree_rampage]+$r_cnt[spree_dom]+$r_cnt[spree_uns]+$r_cnt[spree_god];
 	if (isset($results['debugpid']) && $results['debugpid'] == $pid)
@@ -212,14 +220,15 @@ $rank_matches = $rank_matches+1;
 
 // Work out effective rank given
 $eff_rank = $rank_nrank-$rank_crank;
+if ($rank_year <= 0)
+{
+	// Add effective rank points given to uts_player record --// Timo 13/02/2021 - need to understand whether this needs filtering by year; will be a PITA if so.
+	$sql = "UPDATE uts_player SET rank = '$eff_rank' WHERE id = '$playerecordid';";
+	if (isset($results['debugpid']) && $results['debugpid'] == $pid)
+		$s_debug = $s_debug."-----\r\ntotals-3-add_eff_rank_pts:\r\n".$sql."\r\n";
 
-// Add effective rank points given to uts_player record --// Timo 13/02/2021 - need to understand whether this needs filtering by year; will be a PITA if so.
-$sql = "UPDATE uts_player SET rank = '$eff_rank' WHERE id = '$playerecordid';";
-if (isset($results['debugpid']) && $results['debugpid'] == $pid)
-	$s_debug = $s_debug."-----\r\ntotals-3-add_eff_rank_pts:\r\n".$sql."\r\n";
-
-mysql_query($sql) or die(mysql_error());
-
+	mysql_query($sql) or die(mysql_error());
+}
 // Update the rank
 $sql = "UPDATE uts_rank SET time = '$rank_gametime', rank = '$rank_nrank', prevrank = '$rank_crank', matches = '$rank_matches' WHERE id = '$rank_id' and year = '".$rank_year."';";
 if (isset($results['debugpid']) && $results['debugpid'] == $pid)
